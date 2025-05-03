bb5304c566f4099ad115c51286ac0778d5214cbb
badie16 <badi3bahida16@gmail.com>
2025-05-03 20:31:43 +0100

    update

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div>
+				<h1 class="text-3xl font-bold underline text-red-400">Hello world!</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
