ecd10689ba311a18cdcbd3c59722240f07322445
badie16 <badi3bahida16@gmail.com>
2025-05-03 23:09:59 +0100

    add login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,5 @@
-import { useEffect, useState } from "react";
-import { fetchTest } from "./api";
+import Login from "./pages/login";
 
 export default function App() {
-	const [message, setMessage] = useState("");
-	useEffect(() => {
-		fetchTest().then(setMessage);
-	}, []);
-
-	return (
-		<div className="min-h-screen flex items-center justify-center bg-red-100">
-			<h1 className="text-3xl font-bold">{message || "hors server..."}</h1>
-		</div>
-	);
+	return <Login></Login>;
 }
---
+++ client/src/pages/login.jsx
@@ -0,0 +1,10 @@
+export default function Login() {
+	// badie a said : modifier cette page
+	return (
+		<>
+			<div className="min-h-screen flex items-center justify-center bg-red-100">
+				<h1 className="text-3xl font-bold">Login Page</h1>
+			</div>
+		</>
+	);
+}
