e6f91d2c030f6e56eecca997152150b23067eab7
badie16 <badi3bahida16@gmail.com>
2025-05-08 21:43:24 +0100

    update

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -73,7 +73,7 @@ export default function Login() {
 				</div>
 
 				<div className="mt-auto text-sm opacity-70">
-					© {2025} SoussTalk. Développé par les étudiants de l'ENSIASD
+					© {new Date().getFullYear()} SoussTalk. Développé par les étudiants de l'ENSIASD
 				</div>
 			</div>
 
